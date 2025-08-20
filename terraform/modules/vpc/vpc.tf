b771f911a1a106af14c120a5cc283ca146bb7123
ashish0398 <ashishsunnie@gmail.com>
2025-08-20 23:08:06 +0530

    Refactor code structure for improved readability and maintainability

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -2,6 +2,7 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# Created VPC
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   enable_dns_support = true
@@ -9,6 +10,7 @@ resource "aws_vpc" "main" {
   tags = { Name = "ecs-vpc" }
 }
 
+# Created public subnet.
 resource "aws_subnet" "public" {
   count             = 2
   vpc_id            = aws_vpc.main.id
@@ -17,6 +19,7 @@ resource "aws_subnet" "public" {
   tags              = { Name = "ecs-public-subnet-${count.index}" }
 }
 
+# Created security group for ALB
 resource "aws_security_group" "alb" {
   name        = "alb-sg"
   description = "Allow HTTP/HTTPS traffic"
@@ -41,6 +44,7 @@ resource "aws_security_group" "alb" {
   }
 }
 
+# Created security group for ECS tasks
 resource "aws_security_group" "ecs" {
   name        = "ecs-sg"
   description = "Allow traffic from ALB"
@@ -57,4 +61,33 @@ resource "aws_security_group" "ecs" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# Create an Internet Gateway for internet connectivity
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Create a route table for public subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Associate public subnets with the route table
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public.*.id)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
