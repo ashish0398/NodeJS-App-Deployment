f0c21b84564c6351df822607ea30b0db8b272540
ashish0398 <ashishsunnie@gmail.com>
2025-08-21 20:05:35 +0530

    fix: enhance ALB target group health check configuration for better reliability

--- terraform/modules/alb/alb.tf
+++ terraform/modules/alb/alb.tf
@@ -8,12 +8,18 @@ resource "aws_alb" "main" {
 
 resource "aws_alb_target_group" "app" {
   name        = "nodejs-target-group"
-  port        = var.app_port
+  port        = var.app_port         # 3000
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc_id
+
   health_check {
-    path = "/"
+    path                = "/"        # make sure your app responds here
+    matcher             = "200-399"  # allow success range
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 }
 
@@ -21,6 +27,7 @@ resource "aws_alb_listener" "http" {
   load_balancer_arn = aws_alb.main.arn
   port              = 80
   protocol          = "HTTP"
+
   default_action {
     type             = "forward"
     target_group_arn = aws_alb_target_group.app.arn
